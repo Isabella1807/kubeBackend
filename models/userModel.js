7e611b834d213566e55098cdd77251ffc2ba5dc6
Ann-Cathrine <anncathrinejthrane@gmail.com>
2024-12-02 21:39:40 +0100

    fix create user

--- models/userModel.js
+++ models/userModel.js
@@ -1,27 +1,26 @@
 import kubeDB from "../Database.js";
 
-// create user model som virker med hardcoded data 
-export const createUser = (userData, callback) => {
-    const sql = `INSERT INTO users (uclMail, password, firstName, lastName, roleId, teamId) VALUES (?, ?, ?, ?, ?, ?)`;
-    const values = [
-        userData.uclMail,
-        userData.password,
-        userData.firstName,
-        userData.lastName,
-        userData.roleId,
-        userData.teamId
-    ];
-
-    //  create new user data
-    kubeDB.query(sql, values, (err, result) => {
-        if (err) {
-            console.error("Database error:", err);
-            callback(err, null);
-        } else {
-            callback(null, result);
-        }
-    });
-};
+// function to create user 
+export const createUser = async (userData) => {
+    try {
+      const [result] = await db.query(
+        'INSERT INTO users (uclMail, password, firstName, lastName, roleId, teamId) VALUES (?, ?, ?, ?, ?, ?)',
+        [
+          userData.uclMail,
+          userData.password,
+          userData.firstName,
+          userData.lastName,
+          userData.roleId,
+          userData.teamId
+        ]
+      );
+  
+      return result.insertId;
+    } catch (error) {
+      console.error('Error creating user:', error);
+      throw error;
+    }
+  };
 
 // function find user by id
 export const fetchUserById = (userId, callback) => {

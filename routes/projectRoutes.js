2702660bcae9b0b89bb3fd5603f9a33f40220bbf
cloud141 <kontaktclaudia@gmail.com>
2024-12-06 12:50:52 +0100

    fds

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,8 +2,10 @@ import express from "express";
 import {projectController} from "../controllers/projectController.js";
 import {mustBeLoggedIn} from "../middleware/authenticate.js";
 
+//HUsk at sætte mustbeloggein på nr.1
+
 const router = express.Router();
-router.get("/", projectController.getAll)
+router.get("/",  projectController.getAll)
 router.get("/:id", projectController.getByID)
 router.post("/post", projectController.create)
 router.delete("/:id", projectController.delete)

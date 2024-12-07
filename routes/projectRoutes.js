6c5315c2279d975dde0bcc3ccb66ffbb92624524
cloud141 <kontaktclaudia@gmail.com>
2024-12-07 14:16:54 +0100

    log ud og at den gemmer når man reloader

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,10 +2,10 @@ import express from "express";
 import {projectController} from "../controllers/projectController.js";
 import {mustBeLoggedIn} from "../middleware/authenticate.js";
 
-//HUsk at sætte mustbeloggein på nr.1
+//HUsk at sætte mustBeLoggedIn på nr.1
 
 const router = express.Router();
-router.get("/",  projectController.getAll)
+router.get("/", mustBeLoggedIn, projectController.getAll)
 router.get("/:id", projectController.getByID)
 router.post("/post", projectController.create)
 router.delete("/:id", projectController.delete)

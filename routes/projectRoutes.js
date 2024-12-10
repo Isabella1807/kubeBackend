5cbf1f3f6cd775966d93e5a22c9a34a449236437
Isabella1807 <megaoreomuffin@hotmail.com>
2024-12-10 20:11:36 +0100

    do not delete projects in portainer middleware

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,12 +2,13 @@ import express from "express";
 import {projectController} from "../controllers/projectController.js";
 import {mustBeLoggedIn} from "../middleware/authenticate.js";
 import {requirePortainerAuth} from "../middleware/portainerConnection.js";
+import {STOP_REQUEST} from "../middleware/temp.js";
 
 const router = express.Router();
 router.get("/", mustBeLoggedIn, projectController.getAll)
 router.get("/:id", projectController.getByID)
 router.post("/", mustBeLoggedIn, requirePortainerAuth, projectController.create)
-router.delete("/:id", mustBeLoggedIn, requirePortainerAuth, projectController.delete)
+router.delete("/:id", mustBeLoggedIn, STOP_REQUEST, requirePortainerAuth, projectController.delete)
 router.post("/start/:id", projectController.startProject)
 router.post("/stop/:id", projectController.stopProject)
 router.post("/restart/:id", projectController.restartProject)
